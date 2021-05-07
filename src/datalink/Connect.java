2c85125ac139ec3fd7474c906144cc4a08d11732
Saleh Rezq <salehrezq@gmail.com>
2021-05-07 15:26:33 +0300

    Update Connect with sample code for database
    Example of connection and statement

--- src/datalink/Connect.java
+++ src/datalink/Connect.java
@@ -5,10 +5,62 @@
  */
 package datalink;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.sql.PreparedStatement;
+
 /**
  *
  * @author Saleh
  */
 public class Connect {
+
+    // JDBC driver name and database URL
+    static final String JDBC_DRIVER = "org.mariadb.jdbc.Driver";
+    static final String DB_URL = "jdbc:mariadb://localhost:3306/dabtest";
+
+    //  Database credentials
+    static final String USER = "root";
+    static final String PASS = "36976250";
+
+    public void getConnection() {
+
+        Connection conn = null;
+        Statement stmt = null;
+        try {
+            //STEP 2: Register JDBC driver
+            Class.forName(JDBC_DRIVER);
+            conn = DriverManager.getConnection(DB_URL, USER, PASS);
+
+            stmt = conn.createStatement();
+
+            // Update as required:
+//             String sql = "INSERT INTO employees (`name`,`enrolled_date`, `active`) VALUES ('Mishab ad', '2021-5-1', 0)";
+//            stmt.executeUpdate(sql);
+        } catch (SQLException se) {
+            //Handle errors for JDBC
+            se.printStackTrace();
+        } catch (Exception e) {
+            //Handle errors for Class.forName
+            e.printStackTrace();
+        } finally {
+            //finally block used to close resources
+            try {
+                if (stmt != null) {
+                    conn.close();
+                }
+            } catch (SQLException se) {
+            }// do nothing
+            try {
+                if (conn != null) {
+                    conn.close();
+                }
+            } catch (SQLException se) {
+                se.printStackTrace();
+            }//end finally try
+        }//end try
+    }
 
 }
