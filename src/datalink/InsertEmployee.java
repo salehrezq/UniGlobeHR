b4829d3b36cb1cacff5e320f7b4cd4589978a39c
Saleh Rezq <salehrezq@gmail.com>
2021-05-12 19:39:39 +0300

    Implement insert employee, method not in use yet
    Initial implementation

--- src/datalink/InsertEmployee.java
+++ src/datalink/InsertEmployee.java
@@ -5,10 +5,50 @@
  */
 package datalink;
 
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.Connection;
+import java.time.LocalDate;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  * @author Saleh
  */
 public class InsertEmployee {
 
+    Connect connect;
+    Connection conn;
+
+    public InsertEmployee() {
+        super();
+
+        connect = new Connect();
+    }
+
+    public void insertEmp(String name, LocalDate localDate, boolean active) {
+        try {
+
+            String sql = "INSERT INTO employees (`name`,enrolled_date) VALUES (?, ?, ?)";
+            conn = connect.getConnection();
+            PreparedStatement p = conn.prepareStatement(sql);
+
+            p.setString(1, name);
+            p.setObject(2, localDate);
+            p.setBoolean(3, active);
+            p.executeUpdate();
+
+        } catch (SQLException ex) {
+            Logger.getLogger(InsertEmployee.class.getName()).log(Level.SEVERE, null, ex);
+        } finally {
+            try {
+                if (conn != null) {
+                    conn.close();
+                }
+            } catch (SQLException se) {
+                se.printStackTrace();
+            }
+        }
+    }
 }
