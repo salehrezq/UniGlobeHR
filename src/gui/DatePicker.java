2ac6543b586d5433562911757565b89bc585153c
Saleh Rezq <salehrezq@gmail.com>
2021-08-20 22:56:34 +0300

    Implement DatePicker class

--- src/gui/DatePicker.java
+++ src/gui/DatePicker.java
@@ -5,10 +5,59 @@
  */
 package gui;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.Date;
+import java.util.Properties;
+import org.jdatepicker.impl.JDatePanelImpl;
+import org.jdatepicker.impl.JDatePickerImpl;
+import org.jdatepicker.impl.UtilDateModel;
+
 /**
  *
  * @author Saleh
  */
 public class DatePicker {
+
+    private JDatePickerImpl fDatePicker;
+    private UtilDateModel dateModel;
+    private JDatePanelImpl datePanel;
+    private LocalDate date;
+
+    public DatePicker() {
+
+        super();
+
+        dateModel = new UtilDateModel();
+        Properties p = new Properties();
+        p.put("text.today", "Today");
+        p.put("text.month", "Month");
+        p.put("text.year", "Year");
+        datePanel = new JDatePanelImpl(dateModel, p);
+    }
+
+    public JDatePickerImpl getDatePicker() {
+
+        // Don't know about the formatter, but there it is...
+        fDatePicker = new JDatePickerImpl(datePanel, new DateLabelFormatter());
+        fDatePicker.addActionListener((arg0) -> {
+            Date selectedDate = (Date) fDatePicker.getModel().getValue();
+            if (selectedDate != null) {
+                date = LocalDate.ofInstant(selectedDate.toInstant(), ZoneId.systemDefault());
+            }
+        });
+        return fDatePicker;
+    }
+
+    public void setTodayAsDefault() {
+        LocalDate today = LocalDate.now();
+        dateModel.setDate(today.getYear(), today.getDayOfMonth() - 1, today.getDayOfMonth());
+        dateModel.setSelected(true);
+    }
+
+    public LocalDate getDate() {
+        return this.date;
+    }
 
 }
