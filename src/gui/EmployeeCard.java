608d4bce78ce048d00e44af0f3f59d2d57425464
Saleh Rezq <salehrezq@gmail.com>
2021-08-25 16:31:50 +0300

    Add EmployeeCard class file

---
+++ src/gui/EmployeeCard.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gui;
+
+import javax.swing.JPanel;
+
+/**
+ *
+ * @author Saleh
+ */
+public class EmployeeCard extends JPanel{
+    
+}
