27f44e688600f1355fbeabd67d5bf76da2c4f98e
salehrezq <salehrezq@gmail.com>
2021-11-02 18:04:40 +0300

    Add members for `AttendanceDeduction` model

--- src/model/AttendanceDeduction.java
+++ src/model/AttendanceDeduction.java
@@ -10,5 +10,51 @@ package model;
  * @author Saleh
  */
 public class AttendanceDeduction {
-    
+
+    private int id;
+    private int attendanceId;
+    private String descriptionAR;
+    private String descriptionEN;
+    private double deduction;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getAttendanceId() {
+        return attendanceId;
+    }
+
+    public void setAttendanceId(int attendanceId) {
+        this.attendanceId = attendanceId;
+    }
+
+    public String getDescriptionAR() {
+        return descriptionAR;
+    }
+
+    public void setDescriptionAR(String descriptionAR) {
+        this.descriptionAR = descriptionAR;
+    }
+
+    public String getDescriptionEN() {
+        return descriptionEN;
+    }
+
+    public void setDescriptionEN(String descriptionEN) {
+        this.descriptionEN = descriptionEN;
+    }
+
+    public double getDeduction() {
+        return deduction;
+    }
+
+    public void setDeduction(double deduction) {
+        this.deduction = deduction;
+    }
+
 }
