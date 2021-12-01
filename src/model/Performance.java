ac9a8401905442312953e7f9afd3bdf401704c82
salehrezq <salehrezq@gmail.com>
2021-12-01 19:52:41 +0300

    Populate Performance model with members

--- src/model/Performance.java
+++ src/model/Performance.java
@@ -1,9 +1,75 @@
 package model;
 
+import java.time.LocalDate;
+
 /**
  *
  * @author Saleh
  */
 public class Performance {
+
+    private int id;
+    private int employeeId;
+    private LocalDate date;
+    private String descriptionEN;
+    private String descriptionAR;
+    private double amount;
+    private boolean state;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getEmployeeId() {
+        return employeeId;
+    }
+
+    public void setEmployeeId(int employeeId) {
+        this.employeeId = employeeId;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    public String getDescriptionEN() {
+        return descriptionEN;
+    }
+
+    public void setDescriptionEN(String descriptionEN) {
+        this.descriptionEN = descriptionEN;
+    }
+
+    public String getDescriptionAR() {
+        return descriptionAR;
+    }
+
+    public void setDescriptionAR(String descriptionAR) {
+        this.descriptionAR = descriptionAR;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+
+    public boolean isState() {
+        return state;
+    }
+
+    public void setState(boolean state) {
+        this.state = state;
+    }
 
 }
