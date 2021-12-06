c96faa0d0a454f7988ea91afedf16f7dcb8dc4be
salehrezq <salehrezq@gmail.com>
2021-12-06 17:44:30 +0300

    Add time field to PerformanceInput

--- src/gui/performance/PerformanceInput.java
+++ src/gui/performance/PerformanceInput.java
@@ -3,15 +3,21 @@ package gui.performance;
 import gui.DateListener;
 import gui.DatePicker;
 import gui.EmployeeSelectedListener;
+import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.Font;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.Insets;
 import java.time.LocalDate;
+import javax.swing.JFormattedTextField;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
+import javax.swing.event.DocumentEvent;
+import javax.swing.event.DocumentListener;
 import model.Employee;
 
 /**
@@ -23,16 +29,33 @@ public class PerformanceInput implements EmployeeSelectedListener {
     private JPanel mainPanel;
     private JPanel panelStoryInputs;
     private JPanel panelMetaInputs;
+    private JLabel lbTime;
+    private JFormattedTextField fieldTime;
+    private DatePicker datePicker;
     private JTextField tfTitle;
     private JScrollPane scrollableTextArea;
     private JTextArea taDescription;
-    private DatePicker datePicker;
+    private Color timeRight;
+    private Color timeWrong;
 
     public PerformanceInput() {
 
         panelMetaInputs = new JPanel();
 
         GridBagConstraints c;
+
+        timeRight = new Color(226, 252, 237);
+        timeWrong = new Color(254, 225, 214);
+
+        lbTime = new JLabel("Time:");
+        panelMetaInputs.add(lbTime);
+
+        fieldTime = new JFormattedTextField();
+        fieldTime.setFont(new Font("SansSerif", Font.BOLD, 12));
+        fieldTime.setBackground(timeRight);
+        fieldTime.getDocument().addDocumentListener(new DocumentRegex());
+        fieldTime.setPreferredSize(new Dimension(60, 27));
+        panelMetaInputs.add(fieldTime);
 
         datePicker = new DatePicker();
         datePicker.setTodayAsDefault();
@@ -102,6 +125,33 @@ public class PerformanceInput implements EmployeeSelectedListener {
         public void dateChanged(LocalDate date) {
             System.out.println(date);
         }
+    }
+
+    class DocumentRegex implements DocumentListener {
+
+        private void doWork() {
+            if (fieldTime.getText().matches("^(00|0[0-9]|1[012]):[0-5][0-9] ((a|p)m|(A|P)M)$")) {
+                fieldTime.setBackground(timeRight);
+            } else {
+                fieldTime.setBackground(timeWrong);
+            }
+        }
+
+        @Override
+        public void insertUpdate(DocumentEvent e) {
+            doWork();
+        }
+
+        @Override
+        public void removeUpdate(DocumentEvent e) {
+            doWork();
+        }
+
+        @Override
+        public void changedUpdate(DocumentEvent e) {
+            doWork();
+        }
+
     }
 
 }
