9755bd1d6fc05f06bb4bfd5847d49274796cfd0e
salehrezq <salehrezq@gmail.com>
2021-12-19 23:31:55 +0300

    Setup for CancelListener  in PerformanceEdit

--- src/gui/performance/PerformanceCancel.java
+++ src/gui/performance/PerformanceCancel.java
@@ -2,6 +2,8 @@ package gui.performance;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
+import java.util.List;
 import javax.swing.JButton;
 import javax.swing.JOptionPane;
 
@@ -16,9 +18,11 @@ public class PerformanceCancel
 
     private JButton btnCancel;
     private PerformanceInput performanceInput;
+    private List<CancelListener> cancelledListeners;
 
     public PerformanceCancel() {
 
+        cancelledListeners = new ArrayList<>();
         btnCancel = new JButton("Cancel");
         btnCancel.addActionListener(new SubmitPerformance());
     }
@@ -46,10 +50,22 @@ public class PerformanceCancel
         btnCancel.setEnabled(true);
     }
 
+    public void addCancelListener(CancelListener canceller) {
+        this.cancelledListeners.add(canceller);
+    }
+
+    private void notifyCancelled() {
+        this.cancelledListeners.forEach((canceller) -> {
+            canceller.cancelled();
+        });
+    }
+
     class SubmitPerformance implements ActionListener {
 
         @Override
         public void actionPerformed(ActionEvent e) {
+
+            notifyCancelled();
 
             int dialogResult = JOptionPane.showConfirmDialog(null,
                     "You are about to clear all fields, sure?", "Warning", JOptionPane.YES_OPTION);
