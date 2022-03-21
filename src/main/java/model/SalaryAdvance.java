f4944997e4bcc0c45e7b9ae16df0cd348f7d982f
salehrezq <salehrezq@gmail.com>
2022-03-21 10:54:51 +0300

    Implement SalaryAdvance model class

--- src/main/java/model/SalaryAdvance.java
+++ src/main/java/model/SalaryAdvance.java
@@ -1,9 +1,59 @@
 package model;
 
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.time.YearMonth;
+
 /**
  *
  * @author Saleh
  */
 public class SalaryAdvance {
+
+    private int id;
+    private int employeeId;
+    private YearMonth yearMonthSubject;
+    private LocalDate dateTaken;
+    private BigDecimal amount;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getEmployeeId() {
+        return employeeId;
+    }
+
+    public void setEmployeeId(int employeeId) {
+        this.employeeId = employeeId;
+    }
+
+    public YearMonth getYearMonthSubject() {
+        return yearMonthSubject;
+    }
+
+    public void setYearMonthSubject(YearMonth yearMonthSubject) {
+        this.yearMonthSubject = yearMonthSubject;
+    }
+
+    public LocalDate getDateTaken() {
+        return dateTaken;
+    }
+
+    public void setDateTaken(LocalDate dateTaken) {
+        this.dateTaken = dateTaken;
+    }
+
+    public BigDecimal getAmount() {
+        return amount;
+    }
+
+    public void setAmount(BigDecimal amount) {
+        this.amount = amount;
+    }
 
 }
