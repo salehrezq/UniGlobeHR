e3b65e45b827de040fdb99d1492ca6993c52373b
salehrezq <salehrezq@gmail.com>
2022-03-30 15:43:42 +0300

    Implement UpdateICRPListener interface

--- src/main/java/crud/UpdateICRPListener.java
+++ src/main/java/crud/UpdateICRPListener.java
@@ -1,9 +1,16 @@
 package crud;
 
 /**
+ * UpdateICRPListener; ICRP is abbreviation for Include Current Request
+ * Parameters.
+ *
+ * Update which Include Current Request Parameters; which require different
+ * measurement such as moving the requested record from the current view because
+ * it is no longer relevant to the current request parameters.
  *
  * @author Saleh
  */
 public interface UpdateICRPListener {
 
+    public void updatedICRP();
 }
