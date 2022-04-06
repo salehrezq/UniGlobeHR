6de091a3086dedefe41d10c84d2a9c5d96ad5233
salehrezq <salehrezq@gmail.com>
2022-04-06 23:24:44 +0300

    Initial implementation for Compute class

--- src/main/java/gui/salary/Compute.java
+++ src/main/java/gui/salary/Compute.java
@@ -1,9 +1,33 @@
 package gui.salary;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JButton;
+
 /**
  *
  * @author Saleh
  */
 public class Compute {
+
+    private JButton btnCompute;
+
+    public Compute() {
+
+        btnCompute = new JButton("Compute");
+        btnCompute.addActionListener(new ComputePayables());
+    }
+
+    public JButton getBtnCompute() {
+        return btnCompute;
+    }
+
+    class ComputePayables implements ActionListener {
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            System.out.println("compute place holder");
+        }
+    }
 
 }
