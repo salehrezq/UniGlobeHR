dfb092802e24d5c7d7b1df8a8b9584003069a9b9
salehrezq <salehrezq@gmail.com>
2022-04-09 00:27:07 +0300

    CRUDSalary placeholder implementation

--- src/main/java/datalink/CRUDSalary.java
+++ src/main/java/datalink/CRUDSalary.java
@@ -1,9 +1,31 @@
 package datalink;
 
+import java.sql.Connection;
+import model.Salary;
+import model.SalaryAdvance;
+
 /**
  *
  * @author Saleh
  */
 public class CRUDSalary {
+
+    private static Connection conn;
+
+    public static boolean create(Salary salaryAdvance) {
+        throw new UnsupportedOperationException("CRUDSalary create placeholder");
+    }
+
+    public static Salary getById(int id) {
+        throw new UnsupportedOperationException("CRUDSalary getById placeholder");
+    }
+
+    public static boolean update(Salary salaryAdvance) {
+        throw new UnsupportedOperationException("CRUDSalary update placeholder");
+    }
+
+    public static boolean delete(Integer id) {
+        throw new UnsupportedOperationException("CRUDSalary delete placeholder");
+    }
 
 }
