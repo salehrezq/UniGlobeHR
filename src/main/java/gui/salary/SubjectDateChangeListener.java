6eb714aaf8796281cd6a83907e9179bac7f9ba4f
salehrezq <salehrezq@gmail.com>
2022-04-18 01:46:54 +0300

    Create SubjectDateChangeListener interface

---
+++ src/main/java/gui/salary/SubjectDateChangeListener.java
@@ -0,0 +1,13 @@
+package gui.salary;
+
+/**
+ *
+ * @author Saleh
+ */
+public interface SubjectDateChangeListener {
+
+    public void yearOrMonthChanged();
+
+    public void yearAndMonthNotChanged();
+
+}
