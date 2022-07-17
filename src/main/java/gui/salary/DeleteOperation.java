13f8057753c14b62ef449a179ba3c9bcd8d38ce6
salehrezq <salehrezq@gmail.com>
2022-07-17 17:43:26 +0300

    Create `DeleteOperation` class file

---
+++ src/main/java/gui/salary/DeleteOperation.java
@@ -0,0 +1,21 @@
+package gui.salary;
+
+import model.Salary;
+
+/**
+ *
+ * @author Saleh
+ */
+public class DeleteOperation implements Operation {
+
+    @Override
+    public void switchOperationFor(Subject subject) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean post(Salary salary) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+}
