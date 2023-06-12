9efd2267aa84880eaab1f58637b421d515c1ce5c
salehrezq <salehrezq@gmail.com>
2023-06-12 16:13:36 +0300

    Implement `RotatablePreview` class

--- src/main/java/gui/empinsertdialoge/RotatablePreview.java
+++ src/main/java/gui/empinsertdialoge/RotatablePreview.java
@@ -1,9 +1,76 @@
 package gui.empinsertdialoge;
 
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.geom.AffineTransform;
+import java.awt.image.BufferedImage;
+import javax.swing.JLabel;
+
 /**
  *
  * @author Saleh
  */
-public class RotatablePreview {
+public class RotatablePreview extends JLabel {
+
+    private BufferedImage master;
+    private BufferedImage rotated;
+
+    public RotatablePreview(String s) {
+        super(s);
+    }
+
+    public void setImage(BufferedImage recivedImage) {
+        master = recivedImage;
+    }
+
+    public void rotate(double angle) {
+        rotated = rotateImageByDegrees(master, angle);
+        repaint();
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        if (rotated != null) {
+            Graphics2D g2d = (Graphics2D) g.create();
+            int x = (getWidth() - rotated.getWidth()) / 2;
+            int y = (getHeight() - rotated.getHeight()) / 2;
+            g2d.drawImage(rotated, x, y, this);
+            g2d.dispose();
+        }
+    }
+
+    @Override
+    public Dimension getPreferredSize() {
+        return master == null
+                ? new Dimension(100, 100)
+                : new Dimension(master.getWidth(), master.getHeight());
+    }
+
+    public BufferedImage rotateImageByDegrees(BufferedImage img, double angle) {
+
+        double rads = Math.toRadians(angle);
+        double sin = Math.abs(Math.sin(rads)), cos = Math.abs(Math.cos(rads));
+        int w = img.getWidth();
+        int h = img.getHeight();
+        int newWidth = (int) Math.floor(w * cos + h * sin);
+        int newHeight = (int) Math.floor(h * cos + w * sin);
+
+        BufferedImage rotated = new BufferedImage(newWidth, newHeight, BufferedImage.TYPE_INT_ARGB);
+        Graphics2D g2d = rotated.createGraphics();
+        AffineTransform at = new AffineTransform();
+        at.translate((newWidth - w) / 2, (newHeight - h) / 2);
+
+        int x = w / 2;
+        int y = h / 2;
+
+        at.rotate(rads, x, y);
+        g2d.setTransform(at);
+        g2d.drawImage(img, 0, 0, this);
+        g2d.dispose();
+
+        return rotated;
+    }
 
 }
